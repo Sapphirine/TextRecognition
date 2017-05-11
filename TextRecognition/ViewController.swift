29a61a26ca85ecebd428627fbab0d5db8f64dc04
Fuwy <fwy423@gmail.com>
2017-05-10 22:40:24 -0400

    final swift

--- TextRecognition/ViewController.swift
+++ TextRecognition/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import TesseractOCR
 import AWSCore
 import AWSS3
-import TesseractOCR
+
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
     UINavigationControllerDelegate,
@@ -24,15 +25,8 @@ G8TesseractDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //        if let tesseract = G8Tesseract(language: "eng") {
-        //            tesseract.delegate = self
-        //            tesseract.image = UIImage(named: "line_truth_330")?.g8_blackAndWhite()
-        //            tesseract.recognize()
-        //
-        //            textView.text = tesseract.recognizedText
-        //        }
         
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
         
     }
@@ -120,83 +114,63 @@ G8TesseractDelegate {
         return scaledImage!
     }
     
+    func size_adjustment(image: UIImage, frame_width: CGFloat) -> CGSize {
+        // suppose the width of the fram view is consitant
+        let img_width = image.size.width
+        let img_height = image.size.height
+        
+        var result_size = CGSize()
+        if img_width > img_height {
+            result_size = CGSize(width: frame_width, height: img_height * frame_width / img_width)
+            
+        } else {
+            result_size = CGSize(width: frame_width, height: img_height * frame_width / img_width)
+        }
+        
+        
+        return result_size
+    }
+    
     // image pick control
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [String : Any]) {
         if let selectedPhoto = info[UIImagePickerControllerEditedImage] as? UIImage{
-            //            let scaledImage = scaleImage(image: selectedPhoto, maxDimension: 640)
-            imageView.image = selectedPhoto
+            //            let scaledImage = scaleImage(image: selectedPhoto, maxDimension: 300)
             
             // fix the display photo size
-            imageView.sizeThatFits(selectedPhoto.size)
+            let origin_width = imageView.frame.width
+            imageView.frame.size = size_adjustment(image: selectedPhoto, frame_width: origin_width)
+            
+            imageView.image = selectedPhoto
             
             addActivityIndicator()
             
             dismiss(animated: true, completion: {
-                self.performImageRecognition(image: selectedPhoto)
-                //                self.performImageRecognition(image: scaledImage)
+                let image_url = self.upload_image_to_s3(image: selectedPhoto)
+                self.get_server_ocr(image_url: image_url)
+                //                self.performImageRecognition(image: selectedPhoto) // local OCR
             })
             
         } else if let selectedPhoto = info[UIImagePickerControllerOriginalImage] as? UIImage{
             //            let scaledImage = scaleImage(image: selectedPhoto, maxDimension: 640)
+            
+            // fix the display photo size
+            let origin_width = imageView.frame.width
+            imageView.frame.size = size_adjustment(image: selectedPhoto, frame_width: origin_width)
+            
             imageView.image = selectedPhoto
             
             addActivityIndicator()
             
             dismiss(animated: true, completion: {
-                
-                self.performImageRecognition(image: selectedPhoto)
-                //                self.performImageRecognition(image: scaledImage)
+                let image_url = self.upload_image_to_s3(image: selectedPhoto)
+                self.get_server_ocr(image_url: image_url)
+                //                self.performImageRecognition(image: selectedPhoto) // local OCR
             })
             
         } else {
             imageView.image = nil
         }
-    }
-    
-    func upload_image_to_S3(image: UIImage){
-        
-        // configure congnito for AWS and iOS
-        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:.USEast1,
-                                                                identityPoolId:"us-east-1:30d3c939-13f1-411d-a5ae-97e367d9cbb6")
-        let configuration = AWSServiceConfiguration(region:.USEast1, credentialsProvider:credentialsProvider)
-        
-        AWSServiceManager.default().defaultServiceConfiguration = configuration
-        
-        // upload an image to AWS S3
-        let S3_bucket_name = "abd-text-recognition"
-        let local_file_name = "CNN_lighter.png"
-        let image_url = Bundle.main.url(forResource: "CNN_lighter", withExtension: "png")!
-        
-        let uploadRequest = AWSS3TransferManagerUploadRequest()
-        
-        uploadRequest?.body = image_url
-        uploadRequest?.key = local_file_name
-        uploadRequest?.bucket = S3_bucket_name
-        uploadRequest?.contentType = "image/png"
-        
-        let transferManager = AWSS3TransferManager()
-        
-        transferManager.upload(uploadRequest!).continueWith(executor: AWSExecutor.mainThread(), block: { (task:AWSTask<AnyObject>) -> Any? in
-            
-            if let error = task.error as NSError? {
-                if error.domain == AWSS3TransferManagerErrorDomain, let code = AWSS3TransferManagerErrorType(rawValue: error.code) {
-                    switch code {
-                    case .cancelled, .paused:
-                        break
-                    default:
-                        print("Error uploading: \(String(describing: uploadRequest?.key)) Error: \(error)")
-                    }
-                } else {
-                    print("Error uploading: \(String(describing: uploadRequest?.key)) Error: \(error)")
-                }
-                return nil
-            }
-            
-            let uploadOutput = task.result
-            print("Upload complete for: \(String(describing: uploadRequest?.key))")
-            return nil
-        })
     }
     
     // perform the image recognition locally
@@ -218,11 +192,6 @@ G8TesseractDelegate {
     }
     
     
-    func uploadImage(image: UIImage){
-        
-    }
-    
-    
     // handle showing and removing the view’s activity indicator:
     func addActivityIndicator() {
         activityIndicator = UIActivityIndicatorView(frame: view.bounds)
@@ -238,10 +207,87 @@ G8TesseractDelegate {
     }
     
     
-    // move the elements of the view in order to prevent the keyboard 
+    // move the elements of the view in order to prevent the keyboard
     // from blocking active text fields:
     
+    func upload_image_to_s3(image: UIImage) -> String {
+        // Configure AWS Cognito Credentials
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:.USEast1,
+                                                                identityPoolId:"us-east-1:30d3c939-13f1-411d-a5ae-97e367d9cbb6")
+        let configuration = AWSServiceConfiguration(region:.USEast1, credentialsProvider:credentialsProvider)
+        
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+        
+        // Set up AWS Transfer Manager Request
+        let S3BucketName = "abd-text-recognition"
+        let ext = "png"
+        let localFileName = "CNN_lighter" // local file name here
+        let remoteName = localFileName + "." + ext
+        //let fileName = NSUUID().UUIDString + "." + ext
+        let imageURL = Bundle.main.url(forResource: localFileName, withExtension: ext)!
+        
+        let uploadRequest = AWSS3TransferManagerUploadRequest()
+        uploadRequest?.body = imageURL
+        uploadRequest?.key = remoteName
+        uploadRequest?.bucket = S3BucketName
+        uploadRequest?.contentType = "image/" + ext
+        
+        let transferManager = AWSS3TransferManager.default()
+        
+        // Perform file upload
+        transferManager.upload(uploadRequest!)
+        transferManager.upload(uploadRequest!).continueWith(executor: AWSExecutor.mainThread(), block: { (task:AWSTask<AnyObject>) -> Any? in
+            
+            if let error = task.error as NSError? {
+                if error.domain == AWSS3TransferManagerErrorDomain, let code = AWSS3TransferManagerErrorType(rawValue: error.code) {
+                    switch code {
+                    case .cancelled, .paused:
+                        break
+                    default:
+                        print("Error uploading: \(String(describing: uploadRequest!.key)) Error: \(error)")
+                    }
+                } else {
+                    print("Error uploading: \(String(describing: uploadRequest!.key)) Error: \(error)")
+                }
+                return nil
+            }
+            
+            let uploadOutput = task.result
+            print("Upload complete for: \(String(describing: uploadRequest!.key))")
+            return nil
+        })
+        
+        print(imageURL)
+        
+        return "http://s3.amazonaws.com/" + S3BucketName + "/" + remoteName
+    }
     
+    func get_server_ocr(image_url: String) {
+        // create a post request to the server
+        var request = URLRequest(url: URL(string: "http://ec2-34-207-139-58.compute-1.amazonaws.com:8888/aply_ocr/")!)
+        request.httpMethod = "POST"
+        
+        var responseString = String("No response...")
+        
+        request.httpBody = image_url.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            
+            responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(String(describing: responseString))")
+        }
+        task.resume()
+        
+        textView.text = responseString
+    }
     
     
 }
